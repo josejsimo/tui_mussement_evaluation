523848928c45800c07e62a1fe7facf6ae3d0b97a
jjsimoperales <jjsimoperales@gmail.com>
2022-01-07 12:15:34 +0100

    Added weather api url

--- Config/AppConfig.php
+++ Config/AppConfig.php
@@ -4,6 +4,7 @@ namespace Config;
 
 class AppConfig {
 	public static $TuiMussementApi = 'https://api.musement.com/api/v3/cities.json';
+	public static $WeatherApi = 'http://api.weatherapi.com/v1/forecast.json?key=562cd4913eb84c8fa95161910220501'; 
 }
 
 ?>
