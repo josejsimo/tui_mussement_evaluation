ae0eea23db2d82df12d008d6f03197bb19b7c891
jjsimoperales <jjsimoperales@gmail.com>
2022-01-08 11:58:36 +0100

    Added coding standards

--- Config/AppConfig.php
+++ Config/AppConfig.php
@@ -1,10 +1,29 @@
 <?php
 
+/**
+ * PHP version 7.4
+ *
+ * @category Config_Class
+ * @package  TuiMussement_Evaluation
+ * @author   Author <jjsimoperales@gmail.com>
+ * @license  http://gnu.org/licenses/gpl-3.0.html GNU general public license v3.0
+ */
+
 namespace Config;
 
-class AppConfig {
-	public static $TuiMussementApi = 'https://api.musement.com/api/v3/cities.json';
-	public static $WeatherApi = 'http://api.weatherapi.com/v1/forecast.json?key=562cd4913eb84c8fa95161910220501'; 
+/**
+ * AppConfig
+ *
+ * @category Class
+ * @package  Config
+ * @author   Author <jjsimoperales@gmail.com>
+ * @license  http://gnu.org/licenses/gpl-3.0.html GNU general public license v3.0
+ */
+
+class AppConfig
+{
+public static $tuiMussementApi = 'https://api.musement.com/api/v3/cities.json';
+public static $weatherApi      = 'http://api.weatherapi.com/v1/forecast.json?key=562cd4913eb84c8fa95161910220501';
+
 }
 
-?>
